e36c1240b5f1f56609c5287de1c449a79cf164a6
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-19 23:02:30 +0300

    с усложнённым 6й урок

--- scripts/bot.js
+++ scripts/bot.js
@@ -7,6 +7,7 @@ function game(){
     let num = Math.floor(Math.random() * 100);
     console.log(num);
     let count;
+    let tries = 10;
     function check(){
         let tryNum  = prompt("Угадай число от 1 до 100");
         let answer;
@@ -14,25 +15,38 @@ function game(){
         if(!tryNum && tryNum == null){
             answer = alert("Игра окончена.");
             count = 4;
-            console.log(tryNum);
         }
         else if(!isNumber(tryNum)){
             answer = alert("Введи число!");
         }
         let converted = parseInt(tryNum);
         if (num < converted){
-            answer = alert("Загаданное число меньше");
+            tries--;
+            answer = alert(`Загаданное число меньше. Осталось попыток ${tries}`);
             count = 2;
         }
         else if (num == converted){
-            answer = alert("Поздравляю, Вы угадали!!!");
-            count = 3;
+            answer = confirm("Поздравляю, Вы угадали!!! Играем ещё?");
+            if(answer){
+                game();
+            }
+            return answer;
         }
         else if (num > converted){
-            answer = alert("Загаданное число больше");
+            tries--;
+            answer = alert(`Загаданное число больше. Осталось попыток ${tries}`);
             count = 1;
+            
         }
-        if(count !=3 && count != 4){check()}
+        if(count !=3 && count != 4){
+            if(tries>0){check()}
+            else if(tries == 0){
+                answer = confirm("Попытки закончились. Попробовать снова?");
+                if(answer){
+                    game();
+                }
+            }
+        }
         return answer;
     }    
     console.dir(check);
